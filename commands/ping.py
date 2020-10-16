c62f3396af85f8657f2cc7e69b0e918ae1ff808c
plun1331 <49261529+plun1331@users.noreply.github.com>
2020-10-15 20:13:57 -0700

    Now displays the correct ping

--- commands/ping.py
+++ commands/ping.py
@@ -9,7 +9,7 @@ class PingCMD(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send(f'Pong! **`{self.bot.latency}`**')
+        await ctx.send(f"Pong! **`{round(bot.latency * 1000)}`ms.**")
 
 def setup(bot):
     bot.add_cog(PingCMD(bot))
